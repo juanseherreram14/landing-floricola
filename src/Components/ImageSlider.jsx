465defa3ef77e5a61807d4e7f0627980d8dde16d
123-code <naranjojose256@gmail.com>
2023-09-06 14:45:22 -0500

    About us

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -10,7 +10,7 @@ export default function ImageSlider() {
           <div className="image-overlay"></div>
           <img className="object-scale-down h-47 w-47" src={"https://www.florespedia.com/Imagenes/flores-bonitas-girasoles.jpg"} alt="flores" />
           <div className="header">Florística</div>
-          <div className="subheader">Your vision</div>
+          <div className="subheader">Passion For Roses</div>
         </div>
 
         <div className='image-container'>
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -29,7 +29,10 @@ export default function FlorMain() {
    
       <Navbar />
       <ImageSlider />
+  
       <AboutUs />
+      
+    
       <ButtonWithLogo
         
         logoSrc={logoWhatsapp}
