59c02520c2176ea63005056bd2846fd914353c9a
123-code <naranjojose256@gmail.com>
2023-09-20 22:07:10 -0500

    primeras Imgs

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -8,6 +8,46 @@ import CenteredButton from './SeeMoreBtn'
 import CatalogueItem from './CatalogueItem'
 import Card from './Card';
 import './Accordion.css'
+import explorer1 from '../Images/EXPLORER.jpg'
+import explorer2 from '../Images/EXPLORER.png'
+import explorerflorero from '../Images/EXPLORER.png'
+import br1 from '../Images/BRIGHTON.jpg'
+import br2 from '../Images/BRIGHTON.png'
+import brflorero from '../Images/BRIGHTON-FLORERO.jpg'
+import bl1 from '../Images/BLUEZFLORERO.jpg'
+import bl2 from '../Images/BLUEZ.png'
+import blflorero from '../Images/BLUEZFLORERO.jpg'
+import cl1 from '../Images/BLUEZFLORERO.jpg'
+import cl2 from '../Images/BLUEZ.png'
+import clflorero from '../Images/CANDELIGHT-FLORERO.jpg'
+import dp1 from '../Images/BLUEZFLORERO.jpg'
+import dp2 from '../Images/BLUEZ.png' 
+import dpflorero from '../Images/CANDELIGHT-FLORERO.jpg'
+import ep1 from '../Images/ESPERANCE.jpg'
+import ep2 from '../Images/ESPERANCE.png'
+import epflorero from '../Images/ESPERANCE.jpg'
+import frfl from '../Images/FREEDOM-FLORERO.jpg'
+import fr1 from '../Images/FREEDOM.png'
+import fr2 from '../Images/FREEDOM.jpg'
+import frufl from '../Images/FRUTETTO-FLORERO.jpg'
+import fru1 from '../Images/FRUTETTO.png'
+import fru2 from '../Images/FRUTETTO.jpg'
+import hermf from '../Images/HERMOSA-FLORERO.jpg'
+import herm1 from '../Images/HERMOSA.png'
+import hefl from '../Images/HOT-EXPLORER-FLORERO.jpg'
+import he1 from '../Images/HOTEXPLORER.png'
+import he2 from '../Images/HOTEXPLORER.jpg'
+import kfl from '../Images/HOT-EXPLORER-FLORERO.jpg'
+import k1 from '../Images/HOTEXPLORER.png'
+import k2 from '../Images/HOTEXPLORER.jpg'
+import ll from '../Images/LOLA-FLORERO.jpg'
+import l1 from '../Images/LOLA.png'
+import l2 from '../Images/LOLA.jpg'
+import luf from '../Images/LUCIANO-FLORERO.jpg'
+import lu1 from '../Images/LUCIANO.png'
+import lu2 from '../Images/LUCIANO.jpg'
+
+
 export default function AccordionMenu(){
     const [expanded, setExpanded] = useState(false);
     const [buttonText, setButtonText] = useState("FULL CATALOGUE");
@@ -44,10 +84,12 @@ return(
        
         <AccordionDetails> 
     <h1 className="title"> CATALOGUE </h1>
-          <div style={{alignItems:'center',}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="KAHALA"
           imageSrc="https://i.ibb.co/tLmpDG6/KAHALA.png"
+          imageSrc2={explorer1}
+          imageSrc3 = {explorer2}
           description="Hover over the card to see the description."
         />
 
@@ -56,7 +98,7 @@ return(
             .
           </div>
 
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="NINA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -65,7 +107,7 @@ return(
   
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="MONDIAL"
           imageSrc="https://icecube-us-841.icedrive.io/thumbnail?p=i4YSP1W.pLJIs4R8qma6dH.RR7EOWHQPdXJRYpidusAlU6iV7g339RbrVjexQvn0yO.sD2g5ApON_h0eJfaP0SDlyLGz8uHpUKktTHrYuxDcvWGcR3uOixMrcrnaAhj6.pQBV1ASGuK0gxj72vtdCg--&w=1024&h=1024&m=cropped"
@@ -74,16 +116,18 @@ return(
   
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="BRIGHTON"
-          imageSrc="https://icecube-us-845.icedrive.io/thumbnail?p=BoKekWH5CItzBwOrIFFBqN9Dsmo1.CPs8pQcD0MT9Ay6LEMFsLPq2dJyGYtKgCVqGod.esSAm.fqgB6OBFxYECDlyLGz8uHpUKktTHrYuxDcvWGcR3uOixMrcrnaAhj6.pQBV1ASGuK0gxj72vtdCg--&w=1024&h=1024&m=cropped"
+          imageSrc={brflorero}
+          imageSrc2={br1}
+          imageSrc3 = {br2}
           description="Hover over the card to see the description."
         />
      
 
-          </div>
-          <div style={{display:'flex'}}>
+          </div> 
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="NINA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -92,103 +136,125 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="BLUEZ"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={blflorero}
+          imageSrc2={bl1}
+          imageSrc3 = {bl2}
           description="Hover over the card to see the description."
         />
     
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
            title="CANDELIGHT"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+           imageSrc={clflorero}
+           imageSrc2={cl1}
+           imageSrc3 = {cl2}
           description="Hover over the card to see the description."
         />
     
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="DEEP PURPLE"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={dpflorero}
+          imageSrc2={dp1}
+          imageSrc3 = {dp2}
           description="Hover over the card to see the description."
         />
     
   
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="ESPERANCE"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={epflorero}
+          imageSrc2={ep1}
+          imageSrc3 = {ep2}
           description="Hover over the card to see the description."
         />
       
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="FREEDOM"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={frfl}
+          imageSrc2={fr1}
+          imageSrc3 = {fr2}
           description="Hover over the card to see the description."
         />
      
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="FRUTETTO"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={frufl}
+          imageSrc2={fru1}
+          imageSrc3 = {fru2}
           description="Hover over the card to see the description."
         />
     
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="HERMOSA"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={hermf} 
+          imageSrc2={herm1}
+        
           description="Hover over the card to see the description."
         />
     
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="HOT EXPLORER"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={hefl} 
+          imageSrc2={he1}
+          imageSrc3 = {he2}
           description="Hover over the card to see the description."
         />
     
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="KAHALA"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={kfl} 
+          imageSrc2={k1}
+          imageSrc3 = {k2}
           description="Hover over the card to see the description."
         />
  
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="LOLA"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={ll} 
+          imageSrc2={l1}
+          imageSrc3 = {l2}
           description="Hover over the card to see the description."
         />
   
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="LUCIANO"
-          imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
+          imageSrc={luf} 
+          imageSrc2={lu1}
+          imageSrc3 = {lu2}
           description="Hover over the card to see the description."
         />
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="MAGIC TIMES"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -196,7 +262,7 @@ return(
         />
  
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="MONDIAL"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -205,7 +271,7 @@ return(
   
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="OPALA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -214,7 +280,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="PALOMA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -222,7 +288,7 @@ return(
         />
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="PINK FLOYD"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -231,7 +297,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="PLAYA BLANCA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -240,7 +306,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="PROUD"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -249,7 +315,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="RED PANTHER"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -258,7 +324,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="TIFFANY"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -267,7 +333,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="WHITE OHARA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -276,7 +342,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="NINA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
@@ -285,7 +351,7 @@ return(
 
 
           </div>
-          <div style={{display:'flex'}}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card 
           title="NINA"
           imageSrc="https://i.ibb.co/GFTjgzB/NINA-FLORERO.jpg"
