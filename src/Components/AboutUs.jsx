8d6c0d64a4dbb2e61196ccf35fc66e13086f6e66
JuanseFisaTS <142258092+JuanseFisaTS@users.noreply.github.com>
2023-09-26 14:13:43 -0500

    Cambios About us

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -3,14 +3,13 @@ import "./AboutUs.css";
 import fotoAboutUs from '../Images/aboutUsPhoto.jpg';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import fotof2 from '../Images/aboutUsPhoto.jpg'
-import fotoRosas from '../Images/IMG_0171.jpg';
 import fotoRosas1 from '../Images/IMG_0171.jpg';
 import fotoRosas2 from '../Images/IMG_01712.jpg';
 import fotoRosas3 from '../Images/IMG_01713.jpg';
+
+import fotoRosas from '../Images/FREEDOM.png';
 import { useTranslation } from 'react-i18next'; // Importa useTranslation de react-i18next
 import FormularioPopup from './FormularioPopup'; // Importa el componente del formulario
-import Card from './Card';
 
 const AboutUs = () => {
   const { t } = useTranslation(); // Obtiene t (función de traducción)
@@ -33,46 +32,43 @@ const AboutUs = () => {
         <div className="about-us">
           <div className="about-us-content">
             <div className="left-side">
-              <h1 className="title">{t('aboutUsTitle')}</h1>
+              <h1 className="title mobile-center">{t('aboutUsTitle')}</h1>
               <blockquote className="quote">
-                <p>
-                  {t('aboutUsText')}
-                </p>
-                <button className="btnContacto" onClick={abrirFormulario}>
-                  {t('contactUs')}
-                </button>
+                <p className="mobile-text">{t('aboutUsText')}</p>
               </blockquote>
+              <button className="btnContacto mobile-button" onClick={abrirFormulario}>
+                {t('contactUs')}
+              </button>
             </div>
             <div className="right-side">
-              <img src={fotof2} alt="About Us" className="about-us-image" />
+              <img src={fotoRosas} alt="About Us" className="about-us-image" />
             </div>
           </div>
         </div>
 
-        {/* Segundo slide (idéntico al primero) */}
+        {/* Segundo slide */}
         <div className="about-us">
           <div className="about-us-content">
             <div className="left-side">
-              <h1 className="title">{t('packagesTitle')}</h1>
+              <h1 className="title mobile-center">{t('packagesTitle')}</h1>
               <blockquote className="quote">
-                <p>
-                  {t('packagesText')}
-                </p>
-                <button className="btnContacto" onClick={abrirFormulario}>
-                  {t('contactUs')}
-                </button>
+                <p className="mobile-text">{t('packagesText')}</p>
               </blockquote>
+              <button className="btnContacto mobile-button" onClick={abrirFormulario}>
+                {t('contactUs')}
+              </button>
             </div>
             <div className="right-side">
-           
-            <img src={fotoRosas1} alt="About Us" className="about-us-image" />
-            <img src={fotoRosas2} alt="About Us" className="about-us-image" />
-            <img src={fotoRosas3} alt="About Us" className="about-us-image" />
+              <div className="image-container"> {/* Contenedor de imágenes */}
+                <img src={fotoRosas1} alt="About Us" className="about-us-image" />
+                <img src={fotoRosas2} alt="About Us" className="about-us-image" />
+                <img src={fotoRosas3} alt="About Us" className="about-us-image" />
+              </div>
             </div>
           </div>
         </div>
       </Carousel>
-      
+
       {/* Renderiza el formulario si mostrarFormulario es verdadero */}
       {mostrarFormulario && <FormularioPopup cerrarPopup={() => setMostrarFormulario(false)} mostrar={mostrarFormulario} />}
     </section>
